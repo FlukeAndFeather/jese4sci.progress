fb32a31dcb9ec960f06d1d3e6295ae7b0510d9ea
Max Czapanskiy <maxczap@stanford.edu>
2021-12-02 10:11:14 -0800

    Add documentation

--- R/fetch.R
+++ R/fetch.R
@@ -1,8 +1,25 @@
+#' Fetch all jese4sci issues
+#'
+#' @return `[data.frame]` with columns:
+#'   - github_username
+#'   - track
+#'   - lesson
+#'   - url
+#'   - is_open
+#'
+#' @export
 fetch_issues <- function() {
   repos <- c("jese4sci-RC", "jese4sci-MOD", "jese4sci-DOC", "jese4sci-VAL")
   purrr::map_dfr(repos, fetch_track_issues)
 }
 
+#' Fetch jese4sci issues for one track
+#'
+#' @param repo `[character(1)]` Which jese4sci track repo to fetch issues from.
+#'
+#' @return `[data.frame]` See `fetch_issues()`.
+#'
+#' @keywords internal
 fetch_track_issues <- function(repo = c("jese4sci-RC",
                                         "jese4sci-MOD",
                                         "jese4sci-DOC",
@@ -22,11 +39,21 @@ fetch_track_issues <- function(repo = c("jese4sci-RC",
     track = stringr::str_extract(repo, "[A-Z]+$"),
     lesson,
     url,
-    open
+    is_open
   ) %>%
     dplyr::filter(is_assignment(lesson, repo))
 }
 
+#' Check if issue is an assignment
+#'
+#' @param issue_title `[character]` Vector ofGitHub issue titles. Must all be
+#'   from the same repository.
+#' @param repo `[character(1)]` Repository name e.g. `"jese4sci-RC"`
+#'
+#' @return `[logical]` Same length as `issue_title`, indicating is issue
+#'   corresponds to an assignment or not.
+#'
+#' @keywords internal
 is_assignment <- function(issue_title, repo) {
   track <- stringr::str_extract(repo, "[A-Z]+$")
   stringr::str_detect(issue_title, glue::glue("{track}[0-9]{{3}}"))
